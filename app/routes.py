81720057ba5076e89c984a38b2f2bafcfb9d3368
alexfiel <alexfiel07@gmail.com>
2019-04-18 11:21:51 +0800

    added flask-login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import LoginForm
 
@@ -27,3 +27,34 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/home', methods=["GET", "POST"])
+def hello_world():
+    if request.form:
+        user = User(request.form['email'], request.form['username'], request.form['password'])
+        db.session.add(user)
+        db.session.commit()
+        flash('New File Recorded')
+    return render_template('home.html')
+
+
+@app.route('/view')
+def view_all():
+    return render_template('view_all.html', Students = Students.query.all() )
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+            flash('Please enter all the fields', 'error')
+        else:
+            student = Students(request.form['name'], request.form['city'],
+                               request.form['addr'], request.form['pin'])
+
+            db.session.add(student)
+            db.session.commit()
+            flash('Record was successfully added')
+            return redirect(url_for('view_all'))
+    return render_template('new.html')
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'tH!$isMy$3crEteK3y!'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cto154@localhost:3306/books'
+    SQLALCHEMY_ECHO = True
+
