9afc3577a4eb586b4d92785cd546bb71267e8da0
alexfiel <alexfiel07@gmail.com>
2019-04-19 10:19:14 +0800

    added error handling, created 404 and 500 messages. Configured email for sending errors.

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cto154@localhost:3306/books'
     SQLALCHEMY_ECHO = True
 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['alexfiel07@gmail.com']
